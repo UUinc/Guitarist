84d0afd7e4cbfbf4040778e5f39b689a476c7748
Yahya LAZREK <63449913+UUinc@users.noreply.github.com>
2022-01-12 11:46:07 +0100

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿
+namespace Guitarist
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.Tabs_TB = new System.Windows.Forms.TextBox();
+            this.GenerateBTN = new System.Windows.Forms.Button();
+            this.TitleTXT = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Tabs_TB
+            // 
+            this.Tabs_TB.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.Tabs_TB.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.Tabs_TB.Font = new System.Drawing.Font("Lucida Sans", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Tabs_TB.Location = new System.Drawing.Point(184, 80);
+            this.Tabs_TB.MaximumSize = new System.Drawing.Size(876, 550);
+            this.Tabs_TB.Multiline = true;
+            this.Tabs_TB.Name = "Tabs_TB";
+            this.Tabs_TB.Size = new System.Drawing.Size(438, 225);
+            this.Tabs_TB.TabIndex = 0;
+            // 
+            // GenerateBTN
+            // 
+            this.GenerateBTN.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.GenerateBTN.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.GenerateBTN.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.GenerateBTN.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.GenerateBTN.Font = new System.Drawing.Font("Britannic Bold", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.GenerateBTN.Location = new System.Drawing.Point(308, 342);
+            this.GenerateBTN.Name = "GenerateBTN";
+            this.GenerateBTN.Size = new System.Drawing.Size(198, 40);
+            this.GenerateBTN.TabIndex = 1;
+            this.GenerateBTN.Text = "Generate";
+            this.GenerateBTN.UseVisualStyleBackColor = false;
+            this.GenerateBTN.Click += new System.EventHandler(this.GenerateBTN_Click);
+            // 
+            // TitleTXT
+            // 
+            this.TitleTXT.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.TitleTXT.AutoSize = true;
+            this.TitleTXT.Font = new System.Drawing.Font("Britannic Bold", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TitleTXT.Location = new System.Drawing.Point(301, 9);
+            this.TitleTXT.Name = "TitleTXT";
+            this.TitleTXT.Size = new System.Drawing.Size(205, 53);
+            this.TitleTXT.TabIndex = 2;
+            this.TitleTXT.Text = "Guitarist";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 80);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(10);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(166, 225);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(667, 80);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(10);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(90, 225);
+            this.pictureBox2.TabIndex = 4;
+            this.pictureBox2.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.TitleTXT);
+            this.Controls.Add(this.GenerateBTN);
+            this.Controls.Add(this.Tabs_TB);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Guitarist";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox Tabs_TB;
+        private System.Windows.Forms.Button GenerateBTN;
+        private System.Windows.Forms.Label TitleTXT;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Media;
+using System.Threading;
+
+namespace Guitarist
+{
+    public partial class Form1 : Form
+    {
+        const string NotesPath = @"C:\Users\hp\Desktop\ABC\C#\Guitarist\Resources\Notes\";
+        SoundPlayer[] Notes;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string notesPath;
+            string baseNotes = "eBGDAE";
+            Notes = new SoundPlayer[120];
+
+            for (int i = 0; i < 13; i++)
+            {
+                try
+                {
+                    notesPath = NotesPath + baseNotes[i / 20] + (i - 20 * (i / 20)) + ".wav";
+                    Notes[i] = new SoundPlayer(notesPath);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                    Application.Exit();
+                }
+            }
+        }
+        private void GenerateBTN_Click(object sender, EventArgs e)
+        {
+            string[] Tabs = Tabs_TB.Lines;
+            string _string = "e";
+
+            for (int i = 0; i < Tabs.GetLength(0); i++)
+            {
+                for (int j = 0; j < Tabs[i].Length; j++)
+                {
+                    switch (Tabs[i][j])
+                    {
+                        //Strings:
+                        // string 2 (B string)
+                        case '(':
+                            _string = "B";
+                            break;
+                        case ')':
+                            _string = "e";
+                            break;
+                        // string 3 (G string)
+                        case '[':
+                            _string = "G";
+                            break;
+                        case ']':
+                            _string = "e";
+                            break;
+                        // string 4 (D string)
+                        case '{':
+                            _string = "D";
+                            break;
+                        case '}':
+                            _string = "e";
+                            break;
+                        // string 5 (A string)
+                        case '/':
+                            _string = "A";
+                            break;
+                        case '\\':
+                            _string = "e";
+                            break;
+                        // string 6 (E string)
+                        case '<':
+                            _string = "E";
+                            break;
+                        case '>':
+                            _string = "e";
+                            break;
+                        //Waiting
+                        case '-':
+                            Application.DoEvents();
+                            Thread.Sleep(500);
+                            break;
+                        case '–':
+                            Application.DoEvents();
+                            Thread.Sleep(800);
+                            break;
+                        case '—':
+                            Application.DoEvents();
+                            Thread.Sleep(1000);
+                            break;
+                        //Playing
+                        default:
+                            try
+                            {
+                                int offset = int.Parse(Tabs[i][j].ToString());
+                                int stringIndex = StringToPlay(_string);
+                                Notes[stringIndex * 20 + offset].Play();
+                            }
+                            catch(Exception ex)
+                            {
+                                MessageBox.Show(ex.Message);
+                                return;
+                            }
+                            
+                            break;
+                    }
+                }
+                Application.DoEvents();
+                Thread.Sleep(800);
+            }
+        }
+        int StringToPlay(string noteStr)
+        {
+            string baseNotes = "eBGDAE";
+            for (int i = 0; i < baseNotes.Length; i++)
+            {
+                if (baseNotes[i] == noteStr[0])
+                    return i;
+            }
+            return -1;
+        }
+    }
+}
